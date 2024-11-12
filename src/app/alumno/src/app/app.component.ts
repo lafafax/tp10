be40ebc24d5b23e2165bddeabc1e9d824da7e087
lafafax <142627356+lafafax@users.noreply.github.com>
2024-11-12 15:47:51 -0300

    Add files via upload

---
+++ src/app/alumno/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UsuarioComponent } from './usuario/usuario.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,UsuarioComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'interpolacion';
+}
